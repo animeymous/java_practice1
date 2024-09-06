92217e471ddcee9f8d96433a466c3f5c6179e7ac
Rahul.Kumar <rahul.kumar@tible.com>
2024-09-06 11:22:14 +0530

    printing sum of 2D Array

---
+++ src/PrintSumOfArray2D.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrintSumOfArray2D {
+    public static void printSumOfArray2D(){
+        Scanner sc  = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int[][] matrix = new int[M][N];
+
+        for(int r = 0; r < matrix.length; r++){
+            for(int c = 0; c < matrix[0].length; c++){
+                matrix[r][c] = sc.nextInt();
+            }
+        }
+
+        for(int r = 0; r < matrix.length; r++){
+            int sum = 0;
+            for(int c = 0; c < matrix[0].length; c++){
+                sum = sum + matrix[r][c];
+            }
+            System.out.print(sum+" ");
+        }
+    }
+}
