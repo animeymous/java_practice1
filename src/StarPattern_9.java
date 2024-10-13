e4299f270d6e272229fb492e158dd35259ea43fb
Rahul.Kumar <rahul.kumar@tible.com>
2024-10-13 23:31:01 +0530

    program to print star pattern, day 8

---
+++ src/StarPattern_9.java
@@ -0,0 +1,16 @@
+public class StarPattern_9 {
+    public static void starPattern_9(int row){
+        for(int i = 0; i < row; i++){
+            for(int j = 1; j <= row - i; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < i * 2; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= row - i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
