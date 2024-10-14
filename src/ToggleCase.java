c706f0f9ba33cb2d3aef1e690d3125f7601463c2
Rahul.Kumar <rahul.kumar@tible.com>
2024-10-14 16:27:01 +0530

    program to toggle characters in string, day 28.

---
+++ src/ToggleCase.java
@@ -0,0 +1,15 @@
+public class ToggleCase {
+    public static void toggleCase(String str){
+        String dummyStr = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z'){
+                dummyStr += (char)(str.charAt(i) - 32);
+            }else{
+                dummyStr += (char)(str.charAt(i) + 32);
+            }
+        }
+
+        System.out.print(dummyStr);
+    }
+}
