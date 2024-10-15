1d4a6eded139a43a64f4a33f8033bcf67e51dcc1
Rahul.Kumar <rahul.kumar@tible.com>
2024-10-15 16:24:10 +0530

    program to find maximum sum within window K, Take you forward chanel

---
+++ src/TwoPointerConstantWindow_1.java
@@ -0,0 +1,34 @@
+public class TwoPointerConstantWindow_1 {
+    // Que: Find maximum sum in constant K window (take you forward youtube chanel )
+
+    public static void twoPointerConstantWindow_1(int [] arr, int k){
+        int sum = 0;
+        int maximum_sum = 0;
+
+        //calculate sum till kth element
+        for(int i = 0; i < k; i++){
+            sum = sum + arr[i];
+        }
+
+        maximum_sum = sum;
+
+        System.out.println("max_sum "+ maximum_sum);
+
+        //slide window with L and R two pointer and calculate sum then compare with previous sum
+        int l = 0;
+        int r = k - 1;
+        while(r < arr.length - 1){
+            sum = sum - arr[l];
+            l++;
+            r++;
+            sum = sum + arr[r];
+
+            System.out.println("sum "+ sum);
+            if(sum > maximum_sum){
+                maximum_sum = sum;
+            }
+        }
+
+        System.out.print(maximum_sum);
+    }
+}
