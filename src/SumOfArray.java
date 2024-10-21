14baab420af5442b5fb54966c61a9ec1604be512
Rahul.Kumar <rahul.kumar@tible.com>
2024-10-21 12:52:19 +0530

    added comment

--- src/SumOfArray.java
+++ src/SumOfArray.java
@@ -1,7 +1,9 @@
 public class SumOfArray {
     public static void sumOfArray(int [] arr){
+        //Variable to store final sum of elements
         int sum = 0;
 
+        //Adding sum of elements one by one
         for(int item : arr){
             sum += item;
         }
@@ -9,3 +11,15 @@ public class SumOfArray {
         System.out.println("Sum of given array : "+sum);
     }
 }
+
+
+/*
+Write a program to print sum of elements of the input array A of size N.
+
+Approach:
+1. Create variable to store final sum of elements
+2. In any loop add sum of elements one by one
+
+TC: O(N);
+SC: O(1);
+*/
