39bd44d77e1f914d41944fff383355cb3e0b7b50
Rahul.Kumar <rahul.kumar@tible.com>
2024-12-19 20:56:10 +0530

    program to insert node in link list, day 50, own practice question

--- src/LinkList_1.java
+++ src/LinkList_1.java
@@ -1,2 +1,33 @@
 public class LinkList_1 {
+    public static Link_List_Class insertNode(Link_List_Class head, int value){
+
+        //created new node
+        Link_List_Class newNode = new Link_List_Class(value);
+
+        // created copy of head node, so we don't loose links
+        Link_List_Class temp = head;
+
+        //traverse till last node
+        while(temp != null){
+            temp = temp.next; // by this we can move pointer
+        }
+
+        //linking last node to with new node
+        temp.next = newNode;
+
+        //return head so that we can traverse through this
+        return head;
+    }
 }
+
+/*
+Ques: Program to add insert value in a link list
+
+Approach:
+1. Create new node with same link list class.
+2. Since we are adding then we will add at the end,
+3. traverse till end and make last node point new node and return head
+
+TC: O(N);
+SC: O(N);
+*/
