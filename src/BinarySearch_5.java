279b1c46cef413e2ab90b58a5791012105ba48b5
Rahul.Kumar <rahul.kumar@tible.com>
2024-12-19 20:51:20 +0530

    added link list class to create node

---
+++ src/BinarySearch_5.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class BinarySearch_5 {
+}
---
+++ src/Link_List_Class.java
@@ -0,0 +1,8 @@
+public class Link_List_Class {
+    public int data;
+    public Link_List_Class next;
+    Link_List_Class(int data){
+        this.data = data;
+        next = null;
+    }
+}
