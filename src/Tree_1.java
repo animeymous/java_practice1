0c29bbc07a02146da493fa41cefcb63e91ebc839
Rahul.Kumar <rahul.kumar@tible.com>
2025-01-09 22:57:35 +0530

    added comments

--- src/Tree_1.java
+++ src/Tree_1.java
@@ -2,23 +2,46 @@ import java.util.ArrayList;
 
 public class Tree_1 {
     public static void inOrderTraversal(TreeClass A){
+
+        //arraylist to store elements
         ArrayList<Integer> al = new ArrayList<>();
+
+        //helper function to traverse tree INORDER
         inOrderTraversalHelper(A, al);
 
+        //loop to print values of array
         for (Integer integer : al) {
             System.out.print(integer + " ");
         }
     }
 
+    //helper function implementation
     public static void inOrderTraversalHelper(TreeClass node, ArrayList<Integer> A){
+
+        //base condition for recursion
         if(node == null){
             return;
         }
 
+        // Left
         inOrderTraversalHelper(node.left, A);
+
+        //node
         A.add(node.data);
+
+        //right
         inOrderTraversalHelper(node.right, A);
     }
 }
 
-1 2(4, 5) 3(6, 7)
+/*
+* Ques: Given a binary tree, return the inorder traversal of its nodes' values.
+*
+* Approach:
+* 1: make a arraylist to store elements
+* 2. make helper function with the help of recursion technique to insert elements in array list
+* 3. print array list
+*
+* TC: O(N)
+* SC: O(N)
+*/
