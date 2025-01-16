37c0bfa02a2d281bdb3b245e42a5749093460ecb
Rahul.Kumar <rahul.kumar@tible.com>
2025-01-16 16:24:44 +0530

    Tree class to make nodes for the tree related questions, day 54

--- src/TreeClass.java
+++ src/TreeClass.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TreeClass {
+public class TreeClass {
+
+    //nodes to store address
+    public TreeClass left;
+    public TreeClass right;
+
+    //data variable to store data of each node
+    public int data;
+
+    //constructor function to initialize tree
+    TreeClass(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
